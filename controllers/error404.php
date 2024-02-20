a41c5b37479f67b51ae072bac936a19774913eb0
mggmz <tonykill3@gmail.com>
2024-02-20 08:14:57 -0600

    Created "controllers" folder and added 404 error file.

---
+++ controllers/error404.php
@@ -0,0 +1,20 @@
+<!--
+This file represents the error 404 page.
+It displays a message indicating that the requested page does not exist.
+The user is provided with a button to go back to the home page.
+-->
+
+<?php require "includes/header.php"; ?>
+<div class="d-flex align-items-center justify-content-center vh-100">
+    <div class="text-center">
+        <h1 class="display-1 fw-bold">404</h1>
+        <p class="fs-3"> <span class="text-danger">Opps!</span> Page not found.</p>
+        <p class="lead">
+            The page you’re looking for doesn’t exist.
+        </p>
+        <a href="<?php echo APPURL;  ?>" class="btn btn-primary">Go Home</a>
+    </div>
+</div>
+
+<?php //require "includes/footer.php"; 
+?>
