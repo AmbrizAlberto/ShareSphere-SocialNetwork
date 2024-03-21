c6c3384794023c4c86fb94db922562e7f663eda9
rcasillas2 <rcasillas2@ucol.mx>
2024-03-20 23:05:25 -0600

    Couple changes to the admin view

--- src/views/admin.php
+++ src/views/admin.php
@@ -9,37 +9,38 @@
 </head>
 <body class="container-fluid bg-dark">
     <section class="index_section row">
+        <!-- Here starts the navbar -->
         <nav class="navHome d-flex flex-column flex-shrink-0 bg-dark p-0 border-end" style="width: 4.5rem; position: sticky; height: 100vh; top: 0;">
             <a href="#" class="d-block bg-dark py-3 text-decoration-none mx-auto" data-bs-toggle="tooltip" data-bs-placement="right" title="ShareSphere">
                 <img src="../images/Logo-cut.png" alt="" srcset="" style="width: 45px;">
             </a>
             <ul class="nav nav-pills nav-flush flex-column mb-auto text-center">
                 <li class="nav-item">
-                    <a href="" class="nav-link active py-3 border-bottom rounded-0 bg-dark" data-bs-toggle="tooltip" data-bs-placement="right" title="Home">
+                    <a href="./main.php" class="nav-link active py-3 border-bottom rounded-0 bg-dark" data-bs-toggle="tooltip" data-bs-placement="right" title="Main">
                         <iconify-icon icon="ic:round-home" width="40" height="40"></iconify-icon>
                     </a>
                 </li>
                 <li class="option">
-                    <a href="./inventario/read.php" class="option_container nav-link py-3 border-bottom rounded-0 link-light bg-dark" data-bs-toggle="tooltip" data-bs-placement="right" title="Agua Limpia y Saneamiento">
-                        <img src="../images/6.png" width="30" height="30" alt="Agua Limpia y Saneamiento">
+                    <a href="./main.php" class="option_container nav-link py-3 border-bottom rounded-0 link-light bg-dark" data-bs-toggle="tooltip" data-bs-placement="right" title="Agua Limpia y Saneamiento">
+                        <img src="../images/6.png" width="40" height="40" alt="Agua Limpia y Saneamiento">
                     </a>
                 </li>
                 <li class="option">
-                    <a href="./proveedores/read.php" class="option_container nav-link py-3 border-bottom rounded-0 link-light bg-dark" data-bs-toggle="tooltip" data-bs-placement="right" title="Energía Asequible y no contaminable">
-                        <img src="../images/7.png" width="30" height="30" alt="Energía Asequible y no contaminable">
+                    <a href="./main.php" class="option_container nav-link py-3 border-bottom rounded-0 link-light bg-dark" data-bs-toggle="tooltip" data-bs-placement="right" title="Energía Asequible y no contaminable">
+                        <img src="../images/7.png" width="40" height="40" alt="Energía Asequible y no contaminable">
                     </a>
                 </li>
                
                 <li class="option">
-                    <a href="./movimientos/readProductos.php" class="option_container nav-link py-3 border-bottom rounded-0 link-light bg-dark" data-bs-toggle="tooltip" data-bs-placement="right" title="Vida Submarina">
-                        <img src="../images/14.jpg" width="30" height="30" alt="Vida Submarina">
+                    <a href="./main.php" class="option_container nav-link py-3 border-bottom rounded-0 link-light bg-dark" data-bs-toggle="tooltip" data-bs-placement="right" title="Vida Submarina">
+                        <img src="../images/14.jpg" width="40" height="40" alt="Vida Submarina">
                     </a>
                 </li>
             </ul>
             <div class="border-top dropup">
                 <a href="#" class="d-flex bg-dark align-items-center justify-content-center p-3 link-light text-decoration-none dropdown-toggle"
                     data-bs-toggle="dropdown"  data-bs-offset="10,0">
-                    <iconify-icon class="iconify" icon="mingcute:user-4-fill" width="30" height="30"></iconify-icon>
+                    <iconify-icon class="iconify" icon="lets-icons:user-cicrle-duotone" width="30" height="30"></iconify-icon>
                 </a>
                 <ul class="dropdown-menu">
                     <li><a href="#" class="dropdown-item">Usuario ####</a></li>
@@ -51,8 +52,10 @@
                 </ul>
             </div>
         </nav>
+        <!-- Here ends the navbar -->
 
-        <section class="main_container col-lg-11 bg-dark">
+        <!-- Container with the options starts here -->
+        <section class="main_container col-lg-11 bg-dark">         
             <main class="dashboard_main container m-5">
                 <h2 class="row h1 text-white">ShareSphere</h2>
                 <h4 class="row h2 mb-5 text-white">
@@ -112,6 +115,7 @@
                 </section>
             </main>
         </section>
+        <!-- Container with the options ends here -->
     </section>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
