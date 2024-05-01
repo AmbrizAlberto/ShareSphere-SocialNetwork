712a5ac6b84a47b0df85302f57b8b24df1b54c39
JDAA4 <jaguilar51@ucol.mx>
2024-05-01 00:07:00 -0600

    agregado que si se esta en la pagina de otro usuario, en  el navbar que el boton de profile te redirecione a tu perfil page

--- src/views/userPage.php
+++ src/views/userPage.php
@@ -40,7 +40,7 @@ $userProfile = $posts->GetUserById($_GET['idPerfil']);
         <div class="access">
         <br /><br />
         <a href="./main.php"><button class="optionnv"><i class="bi bi-house-fill"></i></i><span>Home</span></button></a>
-        <a href="#"><button class="optionnv"><i class="bi bi-person-circle"></i></i><span>Profile</span></button></a>
+        <a href="./PerfilPage.php"><button class="optionnv"><i class="bi bi-person-circle"></i></i><span>Profile</span></button></a>
         
       </div>
     </header>
