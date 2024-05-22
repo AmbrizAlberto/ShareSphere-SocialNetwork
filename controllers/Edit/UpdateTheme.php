ce43c1d9179d24b61e534387b0a55ee50349f3e5
JDAA4 <jaguilar51@ucol.mx>
2024-05-21 21:58:21 -0600

    arreglado que al cambiar de tema en los foros no te redirija al main

--- controllers/Edit/UpdateTheme.php
+++ controllers/Edit/UpdateTheme.php
@@ -6,15 +6,7 @@ session_start();
 use Models\posts;
 
 $post = new posts();
-$parts = explode("/", $_GET['CurrentPage']);
-$part = end($parts);
-$page = explode(".", $part)[0];
 $_SESSION['theme'] = ($_SESSION['theme'] === '0') ? '1' : '0';
 $post->UpdateTheme(filter_var($_SESSION['userId']), filter_var($_SESSION['theme']), FILTER_SANITIZE_NUMBER_INT);
-
-if ($page == "main") {
-    header("location:/src/views/main.php");
-} else {
-    header("location:/src/views/PerfilPage.php");
-}
+header("Location:".$_GET['CurrentPage']);
 ?>
